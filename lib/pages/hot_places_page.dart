19012b22b71c88ecbbed6ec0e1b5267cfc67abcc
DevP3030 <wadekarprati3@gmail.com>
2025-03-26 21:26:55 +0530

    Third Stage of Travel App

---
+++ lib/pages/hot_places_page.dart
@@ -0,0 +1,13 @@
+class HotPlacesPage extends StatefulWidget {
+  const HotPlacesPage({super.key});
+
+  @override
+  State<HotPlacesPage> createState() => _HotPlacesPageState();
+}
+
+class _HotPlacesPageState extends State<HotPlacesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
